2c435be81d3d3a572082f15ef08a2800495af897
Christopher Teo <40913834+randomuserhi@users.noreply.github.com>
2024-02-21 00:55:39 +0000

    chore: initial commit

---
+++ ClientDamage/ClientDamage/API/Config.cs
@@ -0,0 +1,35 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+
+namespace ClientDamage {
+    internal static partial class ConfigManager {
+        static ConfigManager() {
+            string text = Path.Combine(Paths.ConfigPath, $"{Module.Name}.cfg");
+            ConfigFile configFile = new ConfigFile(text, true);
+
+            debug = configFile.Bind(
+                "Debug",
+                "enable",
+                false,
+                "Enables debug messages when true.");
+
+            delay = configFile.Bind(
+                "Settings",
+                "delay",
+                0.05f,
+                "Delay in seconds until a limb breaks. Clamped to a value between 0 and 0.2 to prevent abuse.");
+        }
+
+        public static bool Debug {
+            get { return debug.Value; }
+            set { debug.Value = value; }
+        }
+        private static ConfigEntry<bool> debug;
+
+        public static float Delay {
+            get { return delay.Value; }
+            set { delay.Value = value; }
+        }
+        private static ConfigEntry<float> delay;
+    }
+}
---
+++ ClientDamage/ClientDamage/Patches/LimbDestruction.cs
@@ -0,0 +1,51 @@
+﻿using BepInEx.Unity.IL2CPP.Utils.Collections;
+using CharacterDestruction;
+using HarmonyLib;
+using SNetwork;
+using UnityEngine;
+
+namespace ClientDamage.Patches {
+    [HarmonyPatch]
+    internal static class LimbDestruction {
+        private static System.Collections.IEnumerator DelayLimbDestruction(Dam_EnemyDamageLimb limb, sDestructionEventData data) {
+            yield return new WaitForSeconds(Mathf.Clamp(ConfigManager.Delay, 0, 0.2f));
+            callOriginal = true;
+            limb.DestroyLimb(data);
+            callOriginal = false;
+        }
+
+        private static bool callOriginal = false;
+        [HarmonyPatch(typeof(Dam_EnemyDamageLimb), nameof(Dam_EnemyDamageLimb.DestroyLimb))]
+        [HarmonyPrefix]
+        private static bool Prefix_ReceiveDestroyLimb(Dam_EnemyDamageLimb __instance, sDestructionEventData destructionEventData) {
+            if (!SNet.IsMaster || ConfigManager.Delay == 0) return true;
+
+            if (!callOriginal) {
+                __instance.StartCoroutine(DelayLimbDestruction(__instance, destructionEventData).WrapToIl2Cpp());
+            }
+            return callOriginal;
+        }
+    }
+
+    [HarmonyPatch]
+    internal static class CustomLimbDestruction {
+        private static System.Collections.IEnumerator DelayLimbDestruction(Dam_EnemyDamageLimb_Custom limb, sDestructionEventData data) {
+            yield return new WaitForSeconds(Mathf.Clamp(ConfigManager.Delay, 0, 0.2f));
+            callOriginal = true;
+            limb.DestroyLimb(data);
+            callOriginal = false;
+        }
+
+        private static bool callOriginal = false;
+        [HarmonyPatch(typeof(Dam_EnemyDamageLimb_Custom), nameof(Dam_EnemyDamageLimb_Custom.DestroyLimb))]
+        [HarmonyPrefix]
+        private static bool Prefix_ReceiveDestroyLimb(Dam_EnemyDamageLimb_Custom __instance, sDestructionEventData destructionEventData) {
+            if (!SNet.IsMaster || ConfigManager.Delay == 0) return true;
+
+            if (!callOriginal) {
+                __instance.StartCoroutine(DelayLimbDestruction(__instance, destructionEventData).WrapToIl2Cpp());
+            }
+            return callOriginal;
+        }
+    }
+}
